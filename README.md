4fd3dd814ff8556e6d6dfc1e12fc2634153d3c43
Prathamesh Mandke <prathrules@gmail.com>
2018-10-04 00:45:52 +0530

    Functional but crude

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ The credit for the following files is due to [nothings/stb](https://github.com/n
 
 1. stb_image.h
 2. stb_image_write.h
+
+Status -> Basic functionality done.
+
+TODO:
+1. Adopt for other images of various pixel size through commandline args.
+2. Test and verfiy for various images.
+3. Change q50 dynamically and observe compression.
+4. Web based interface.
--- main.c
+++ main.c
@@ -36,9 +36,9 @@ void idct(int *inp[],int *outp[]);
 
 int main(int argc, char const *argv[]){
   int x=360,y=240,n=1;
-  unsigned char *data = stbi_load("img_00031.bmp", &x, &y, &n, 1);
+  unsigned char *data = stbi_load("img_00007.bmp", &x, &y, &n, 1);
 
-/*****
+  /*****
   for(int i = 0; i < 8; i++)
     printf("%d %d\n",i,(int)data[360*8+16+i]);
   ***/
@@ -61,19 +61,17 @@ int main(int argc, char const *argv[]){
   int *rlc = (int *)malloc(128*sizeof(int));
   for(int i =0;i < 128;i++) rlc[i] = -255;
 
-int koi = 0;
+  int koi = 0;
 
   for(int i1 = 0;(i1+8) <= h;i1 += 8)
   for(int j1 = 0;(j1+8) <= w;j1 += 8)
   {
     for(int u1 = 0;u1 < 8;u1++)
     for(int v1 = 0;v1 < 8;v1++)
-    {
       bl[u1][v1] = (int)(data[360*i1 + j1 + 360*u1 + v1]) - 128;
       //if(i1 == 0 && j1 == 0) printf("%d ",bl[u1][v1]+128);
-    }
 
-
+    #ifdef TST
     if(i1 == 0 && j1 == 0){
       for(int u1 = 0;u1 < 8;u1++){
       for(int v1 = 0;v1 < 8;v1++)
@@ -83,8 +81,10 @@ int koi = 0;
       printf("\n");
       }
     }
+    #endif
 
     dct(bl,im_dct);
+    #ifdef TST
     if(i1 == 0 && j1 == 0){
       for(int u1 = 0;u1 < 8;u1++){
       for(int v1 = 0;v1 < 8;v1++)
@@ -94,40 +94,57 @@ int koi = 0;
       printf("\n");
       }
     }
-    //qtize(im_dct,im_qtized);
+    #endif
+
+    qtize(im_dct,im_qtized);
+    #ifdef TST
     if(i1 == 0 && j1 == 0){
-        qtize(im_dct,im_qtized);
       for(int u1 = 0;u1 < 8;u1++){
       for(int v1 = 0;v1 < 8;v1++)
       {
-        printf("%d ",im_qtized[u1][v1]);
+        printf("%d  ",q50[u1][v1]);
       }
       printf("\n");
       }
     }
+    #endif
 
     for(int i =0;i < 128;i++) rlc[i] = -255;
 
-    //rl_code(im_qtized,rlc);
+    rl_code(im_qtized,rlc);
+    #ifdef TST
     if(i1 == 0 && j1 == 0){
         rl_code(im_qtized,rlc);
-      for(int u1 = 0;u1 < 85;u1++) printf("%d ",rlc[u1]);
+      for(int u1 = 0;u1 < 128;u1++) printf("%d ",rlc[u1]);
+      printf("\n");
     }
-#ifdef STTS
+    if(i1 == 0 && j1 == 0){
+
+      printf("\nInflated\n");
+      for(int u1 = 0;u1 < 8;u1++){
+      for(int v1 = 0;v1 < 8;v1++)
+        printf("%d ",im_rec[u1][v1]);
+      printf("\n");
+    }
+    }
+    #endif
+
     inflate_rlc(rlc,im_rec);
 
     idct(im_rec,im_qtized); //stored image idct and added 128
-    if(i1 == 0 && j1 == 0) printf("\n");
+
+    //if(i1 == 0 && j1 == 0) printf("\n");
+
     for(int u1 = 0;u1 < 8;u1++)
     for(int v1 = 0;v1 < 8;v1++)
     {
-      im_compr[360*i1 + j1 + 360*u1 + v1] = (char)(im_dct[u1][v1] + 128);
-      //if(i1 == 0 && j1 == 0) printf("%d ",im_compr[360*i1 + j1 + 360*u1 + v1]-128);
+      im_compr[360*i1 + j1 + 360*u1 + v1] = (char)(im_qtized[u1][v1] + 128);
+      //if(i1 == 0 && j1 == 0) printf("%d %d ;",im_compr[360*i1 + j1 + 360*u1 + v1]-128,(int)(data[360*i1 + j1 + 360*u1 + v1]));
     }
+
   }
   stbi_write_bmp("jpeg_comp.bmp", 360, 240, 1, im_compr);
-#endif
-}
+
   stbi_image_free(data);
   for(int i = 0;i < 8;i++){
   free(bl[i]);
@@ -174,6 +191,112 @@ void qtize(float *inp[],int *outp[]){
     //if(i == 0) printf("is %d ",outp[i][j]);
   }
 }
+
+
+void move_dn(int *l, int *m, int k, int *t, int *inp[]){
+  //printf("down began %d %d\n",i,j);
+  int i=*l,j=*m;
+  t[0] = inp[i++][j--];
+	int cnt = 1;
+	while(cnt <= k) t[cnt++] = inp[i++][j--];
+  //for(int z = 0;z <= k;z++) printf(" down %d ",t[z]);
+	//printf("return dn i=%d j=%d\n",i,j);
+  i--;
+	j++;
+  *l = i;
+  *m = j;
+}
+
+void move_up(int *l, int *m, int k, int *t, int *inp[]){
+  //printf("up began %d %d\n",i,j);
+  int i=*l,j=*m;
+  t[0] = inp[i--][j++];
+	int cnt = 1;
+	while(cnt <= k)	t[cnt++] = inp[i--][j++];
+  //for(int z = 0;z <= k;z++) printf(" up %d i=%d j=%d ",t[z],i,j);
+  //printf("\n");
+  i++;
+	j--;
+  *l = i;
+  *m = j;
+}
+
+void rl_code(int *inp[],int *outp){
+  int *st = (int *)malloc(64*sizeof(int));
+  st[0] = inp[0][0];
+	int k = 1, lp = 1,i = 0,j = 1;
+	int *tmp = (int *)malloc(8*sizeof(int));
+
+	for(;k <= 7;)
+	{
+		if(k%2){
+			move_dn(&i,&j,k,tmp,inp);
+			for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			if(k == 7){
+				i = 7;
+				j = 1;
+				k = 6;
+				break;
+			}
+			i++;
+			j = 0;
+			k++;
+		}
+		else{
+			move_up(&i,&j,k,tmp,inp);
+			for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			i = 0;
+			j++;
+			k++;
+		}
+	}
+	k = 6;
+	i = 7;
+	j = 1;
+	for(;k >= 1;)
+	{
+		if(k%2 == 0){
+				move_up(&i,&j,k,tmp,inp);
+				for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+				//st[lp++] = inp[i-1][7];
+				k--;
+				j = 7;
+				i++;
+		}
+		else{
+			move_dn(&i,&j,k,tmp,inp);
+			for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			if(k == 1){
+				st[lp++] = inp[7][7];
+				break;
+			}
+			i = 7;
+			j++;
+			k--;
+    }
+  }
+  free(tmp);
+  //printf("\n\n\n");
+  //for(int  i = 0;i < 64;i++) printf("%d ",st[i]);
+  //printf("\n\n\n");
+  j = 0;
+  lp  = 1;
+  outp[0] = st[0];
+  for(int i = 1; i < 64;i++)
+  if(st[i]){
+    //for(int k = 0;k < j;k++) outp[lp++] = 0;
+    outp[lp++] = j;
+    j = 0;
+    outp[lp++] = st[i];
+  }
+  else j++;
+
+  free(st);
+
+}
+
+
+#ifdef TST
 
 void rl_code(int *inp[],int *outp){
   int k = 0,z_cnt=0;
@@ -301,7 +424,113 @@ void rl_code(int *inp[],int *outp){
 
 }
 
+#endif
 
+void inflate_rlc(int *inp, int *outp[])
+{
+  //printf("Core dumped?\n");
+  int i,j,lp;
+  int *st = (int *)malloc(64*sizeof(int));
+  i = 1;
+  lp = 1;
+  int cl = 0;
+  st[0] = inp[0];
+
+  while(inp[i] != -255 && i < 129){
+    if(!cl)
+      for(j = 0,cl = 1,i++; j < inp[i-1];j++) st[lp++] = 0;
+    else{
+      st[lp++] = inp[i++];
+      cl = 0;
+    }
+  }
+  //printf("%d %d ",i,lp);
+  for(;lp < 64;lp++) st[lp] = 0;
+  //printf("\n\n\n\n");
+  //for(i = 0;i < 64;i++) printf("st %d   ",st[i]);
+  //printf("\n\n\n\n");
+  for(int u1 = 0;u1 < 8;u1++)
+  for(int v1 = 0;v1 < 8;v1++)
+    outp[u1][v1] = 0;
+  outp[0][0] = inp[0];
+  int k = 1;
+  i = 0;
+  j = 1;
+  lp = 1;
+  for(;k <= 7;)
+	{
+		if(k%2){
+			//move_dn(&i,&j,k,tmp,inp);
+      outp[i++][j--] = st[lp++];
+    	int cnt = 1;
+      //printf("%d %d\n",i,j);
+    	for(;cnt <= k;cnt++) outp[i++][j--] = st[lp++];
+      //printf("Core dumped?\n");
+			//for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			if(k == 7){
+				i = 7;
+				j = 1;
+				k = 6;
+				break;
+			}
+			//i++;
+			j = 0;
+			k++;
+		}
+		else{
+			//move_up(&i,&j,k,tmp,inp);
+      outp[i--][j++] = st[lp++];
+    	int cnt = 1;
+    	for(;cnt <= k;cnt++)	outp[i--][j++] = st[lp++];
+			//for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			i = 0;
+			//j++;
+			k++;
+		}
+	}
+
+	k = 6;
+	i = 7;
+	j = 1;
+	for(;k >= 1;)
+	{
+		if(k%2 == 0){
+				//move_up(&i,&j,k,tmp,inp);
+        outp[i--][j++] = st[lp++];
+      	int cnt = 1;
+      	for(;cnt <= k;cnt++)	outp[i--][j++] = st[lp++];
+  			//for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+				//st[lp++] = inp[i-1][7];
+				k--;
+				j = 7;
+				i += 2;
+		}
+		else{
+			//move_dn(&i,&j,k,tmp,inp);
+      outp[i++][j--] = st[lp++];
+    	int cnt = 1;
+    	for(;cnt <= k;cnt++) outp[i++][j--] = st[lp++];
+			//for(int l = 0;l <= k;l++) st[lp++] = tmp[l];
+			if(k == 1){
+				outp[7][7] = st[lp++];
+				break;
+			}
+			i = 7;
+			j += 2;
+			k--;
+    }
+  }
+  #ifdef TST
+  for(int u1 = 0;u1 < 8;u1++){
+  for(int v1 = 0;v1 < 8;v1++)
+    printf("%d ",outp[u1][v1]);
+  printf("\n");
+  }
+  #endif
+  free(st);
+}
+
+#ifdef TST
 void inflate_rlc(int *inp,int *outp[]){
   int lp = 1, cl = 0;
 
@@ -511,6 +740,7 @@ void inflate_rlc(int *inp,int *outp[]){
   }
 
 }
+#endif
 
 void idct(int *inp[],int *outp[]){
   int i,j,u,v;
@@ -522,7 +752,7 @@ void idct(int *inp[],int *outp[]){
           if(!u && !v) k = 0.5;
           else if((!u && v) || (u && !v)) k = 1/sqrt(2);
           else k = 1;
-          csum += k*inp[u][v]*q50[u][v]*cos((2*i+1)*M_PI*u/(16))*cos((2*j+1)*M_PI*v/(16));
+          csum += k*inp[u][v]*cos((2*i+1)*M_PI*u/(16))*cos((2*j+1)*M_PI*v/(16));
         }
       outp[i][j] = (int)csum/4;
       csum = 0;
